ec8f97aa4852f01748a6cf49c9fddae91177a252
meleane-m-t <meleane_tov@student.asdah.school.nz>
2022-05-03 15:52:12 -0900

    I have added my second question to the coding

--- quiz.py
+++ quiz.py
@@ -1,6 +1,9 @@
 #Introduction
 print("Hello! Welcome to the bible quiz by Meleane")
 
+name = input("Enter your Name: ")
+print("Welcome {}".format(name))
+
 q1 = input("1. In what city was Jesus Born?\na) Nazareth \nb) Bethlehem \nc) Gethsamane \nd) Egypt\n>> ")
 
 if q1 == "a" or q1 == "Nazareth":
@@ -14,3 +17,15 @@ elif q1 == "d" or "Egypt":
 else:
     print("That is not an option")
 
+q2 = input("2. After Jesus was arrested, which apostle disowned him three time?\na) Judas Iscariot \nb) John \nc) Thomas \nd) Peter\n>> ")
+
+if q2 == "a" or q1 == "Judas Iscariot":
+    print("Incorrect")
+elif q1 == "b" or q1 == "John":
+    print("Incorrect")
+elif q1 == "c" or "Thomas":
+    print("Incorrect")
+elif q1 == "d" or "Peter":
+    print("Correct")
+else:
+    print("That is not an option")
