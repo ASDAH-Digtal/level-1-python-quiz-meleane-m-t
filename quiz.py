a770743c546cc9f953f31b9cae71d2dc8f6b3ffb
meleane-m-t <meleane_tov@student.asdah.school.nz>
2022-05-16 10:41:05 +1200

    I have changed all my if and else statements

--- quiz.py
+++ quiz.py
@@ -3,131 +3,146 @@ print("Hello! Welcome to the bible quiz by Meleane")
 
 #Input 
 name = input("Enter your Name: ")
-print("Welcome {}".format(name))
+print("Welcome {}!".format(name))
+print("\n")
 
-q1 = input("1. In what city was Jesus Born?\na) Nazareth \nb) Bethlehem \nc) Gethsamane \nd) Egypt\n>> ")
+score = 0
+
+# QUESTION 1
+q1 = input("1. In what city was Jesus Born?\na) Nazareth \nb) Bethlehem \nc) Gethsamane \nd) Egypt\nAnswer: ")
 
 #If-and-else statements
-if q1 == "a" or q1 == "Nazareth":
-    print("Incorrect")
-elif q1 == "b" or q1 == "Bethlehem":
-    print("Correct")
-elif q1 == "c" or q1 == "Gethsamane":
-    print("Incorrect")
-elif q1 == "d" or q1 == "Egypt":
-    print("Incorrect")
+if q1 =="b" or q1 =="Bethlehem":
+    score += 1 
+    print("Correct!")
+    print("Score: ", score)
+    print("\n")
 else:
-    print("That is not an option")
+    print("Incorrect! The answer is b. Bethlehem")
+    print("Score: ", score)
+    print("\n")
 
-q2 = input("2. After Jesus was arrested, which apostle disowned him three time?\na) Judas Iscariot \nb) John \nc) Thomas \nd) Peter\n>> ")
+# QUESTION 2
+q2 = input("2. After Jesus was arrested, which apostle disowned him three time?\na) Judas Iscariot \nb) John \nc) Thomas \nd) Peter\nAnswer: ")
 
-if q2 == "a" or q2 == "Judas Iscariot":
-    print("Incorrect")
-elif q2 == "b" or q2 == "John":
-    print("Incorrect")
-elif q2 == "c" or q2 == "Thomas":
-    print("Incorrect")
-elif q2 == "d" or q2 == "Peter":
-    print("Correct")
+if q2 =="d" or q2 =="Peter":
+    score += 1 
+    print("Correct!")
+    print("Score: ", score)
+    print("\n")
 else:
-    print("That is not an option")
+    print("Incorrect! The answer is d. Peter")
+    print("Score: ", score)
+    print("\n")
 
-q3 = input("3. How many books are there in the old testament?\na) 27 \nb) 40 \nc) 38 \nd) 39\n>> ")
+#QUESTION 3
+q3 = input("3. How many books are there in the old testament?\na) 27 \nb) 40 \nc) 38 \nd) 39\nAnswer: ")
 
-if q2 == "a" or q3 == "27":
-    print("Incorrect")
-elif q3 == "b" or q3 == "40":
-    print("Incorrect")
-elif q3 == "c" or q3 == "38":
-    print("Incorrect")
-elif q3 == "d" or q3 == "39":
-    print("Correct")
+if q3 =="d" or q3 =="39":
+    score += 1 
+    print("Correct!")
+    print("Score: ", score)
+    print("\n")
 else:
-    print("That is not an option")
+    print("Incorrect! The answer is d. 39")
+    print("Score: ", score)
+    print("\n")
 
-q4 = input("4. What is the shortest book in the New Testament?\na) 1 John \nb) 2 John \nc) 3 John\n>> ")
+# QUESTION 4
+q4 = input("4. What is the shortest book in the New Testament?\na) 1 John \nb) 2 John \nc) 3 John\nAnswer: ")
 
-if q4 == "a" or q4 == "1 John":
-    print("Incorrect")
-elif q4 == "b" or q4 == "2 John":
-    print("Correct")
-elif q4 == "c" or q4 == "3 John":
-    print("Incorrect")
+if q4 =="b" or q4 =="2 John":
+    score += 1 
+    print("Correct!")
+    print("Score: ", score)
+    print("\n")
 else:
-    print("That is not an option")
+    print("Incorrect! The answer is b. 2 John")
+    print("Score: ", score)
+    print("\n")
 
-q5 = input("5. Who is the author of the book of Revelation?\na) John \nb) Joseph \nc) Paul \nd) Timothy\n>> ")
+# QUESTION 5
+q5 = input("5. Who is the author of the book of Revelation?\na) John \nb) Joseph \nc) Paul \nd) Timothy\nAnswer: ")
 
-if q5 == "a" or q5 == "John":
-    print("Correct")
-elif q5 == "b" or q5 == "Joseph":
-    print("Incorrect")
-elif q5 == "c" or q5 == "Paul":
-    print("Incorrect")
-elif q5 == "d" or q5 == "Timothy":
-    print("Incorrect")
+if q5 =="a" or q5 =="John":
+    score += 1 
+    print("Correct!")
+    print("Score: ", score)
+    print("\n")
 else:
-    print("That is not an option")
+    print("Incorrect! The answer is a. John")
+    print("Score: ", score)
+    print("\n")
 
-q6 = input("6. What language was most of the Old Testament originally written in?\na) Greek \nb) Hebrew \nc) Latin \nd) Aramaic\n>> ")
+# QUESTION 6
+q6 = input("6. What language was most of the Old Testament originally written in?\na) Greek \nb) Hebrew \nc) Latin \nd) Aramaic\nAnswer: ")
 
-if q6 == "a" or q6 == "Greek":
-    print("Incorrect")
-elif q6 == "b" or q6 == "Hebrew":
-    print("Correct")
-elif q6 == "c" or q6 == "Latin":
-    print("Incorrect")
-elif q6 == "d" or q6 == "Aramaic":
-    print("Incorrect")
+if q6 =="b" or q6 =="Hebrew":
+    score += 1 
+    print("Correct!")
+    print("Score: ", score)
+    print("\n")
 else:
-    print("That is not an option")
+    print("Incorrect! The answer is b. Hebrew")
+    print("Score: ", score)
+    print("\n")
 
-q7 = input("Matthew was a _______?\na) Tax Collector \nb) Fisherman \nc) Carpenter \nd) Murderer\n>> ")
+# QUESTION 7
+q7 = input("Matthew was a _______?\na) Tax Collector \nb) Fisherman \nc) Carpenter \nd) Murderer\nAnswer: ")
 
-if q7 == "a" or q7 == "Tax Collector":
-    print("Correct")
-elif q7 == "b" or q7 == "Fisherman":
-    print("Incorrect")
-elif q7 == "c" or q7 == "Carpenter":
-    print("Incorrect")
-elif q7 == "d" or q7 == "Murderer":
-    print("Incorrect")
+if q7 =="a" or q7 =="Tax Collector":
+    score += 1 
+    print("Correct!")
+    print("Score: ", score)
+    print("\n")
 else:
-    print("That is not an option")
+    print("Incorrect! The answer is a. Tax Collector")
+    print("Score: ", score)
+    print("\n")
 
-q8 = input("Why did God put a mark on Cain?\na) To curse him \nb) To shame him \nc) To protect him \nd) To mark him for slaughter\n>> ")
+# QUESTION 8
+q8 = input("Why did God put a mark on Cain?\na) To curse him \nb) To shame him \nc) To protect him \nd) To mark him for slaughter\nAnswer: ")
 
-if q8 == "a" or q8 == "To curse him":
-    print("Incorrect")
-elif q8 == "b" or q8 == "To shame him":
-    print("Incorrect")
-elif q8 == "c" or q8 == "To protect him":
-    print("Correct")
-elif q8 == "d" or q8 == "To mark him for slaughter":
-    print("Incorrect")
+if q8 =="c" or q8 =="To protect him":
+    score += 1 
+    print("Correct!")
+    print("Score: ", score)
+    print("\n")
 else:
-    print("That is not an option")
+    print("Incorrect! The answer is c. To protect him")
+    print("Score: ", score)
+    print("\n")
 
-q9 = input("Name the place where Jesus walked on water?\na) Sea of Galilee \nb) Jordan River \nc) The Red Sea\n>> ")
+# QUESTION 9
+q9 = input("Name the place where Jesus walked on water?\na) Sea of Galilee \nb) Jordan River \nc) The Red Sea\nAnswer: ")
 
-if q7 == "a" or q7 == "Sea of Galilee":
-    print("Correct")
-elif q7 == "b" or q7 == "Jordan River":
-    print("Incorrect")
-elif q7 == "c" or q7 == "The Red Sea":
-    print("Incorrect")
+if q9 =="a" or q9 =="Sea of Galilee":
+    score += 1 
+    print("Correct!")
+    print("Score: ", score)
+    print("\n")
 else:
-    print("That is not an option")
+    print("Incorrect! The answer is a. Sea of Galilee")
+    print("Score: ", score)
+    print("\n")
 
-q10 = input("Which book did Jesus directly write?\na) Matthew \nb) Mark \nc) Luke \nd) John \ne) None\n>>")
+# QUESTION 10
+q10 = input("Which book did Jesus directly write?\na) Matthew \nb) Mark \nc) Luke \nd) John \ne) None\nAnswer: ")
 
-if q10 == "a" or q10 == "Sea of Galilee":
-    print("Incorrect")
-elif q10 == "b" or q10 == "Jordan River":
-    print("Incorrect")
-elif q10 == "c" or q10 == "The Red Sea":
-    print("Incorrect")
-elif q10 == "e" or q10 == "None": 
-    print("Correct")
+if q9 =="e" or q9 =="none" or q9 =="None":
+    score += 1 
+    print("Correct!")
+    print("Score: ", score)
+    print("\n")
 else:
-    print("That is not an option")
+    print("Incorrect! The answer is e. None")
+    print("Score: ", score)
+    print("\n")
+
+# FINAL MESSAGE
+if score <= 5:
+    print ("Your total score is:", score, "- Not bad!")
+elif score == 8:
+    print("Your total score is:", score, "- Well done!")
+else:
+    print("Your total score is", score, "- You are awesome!")
